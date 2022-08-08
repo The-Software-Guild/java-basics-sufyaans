490ca662af1093dc76bf894aee31a4d0531d2787
sufyaans <ss1224@student.le.ac.uk>
2022-08-08 11:21:58 +0100

    Final

--- MySQL/shoppingList.sql
+++ MySQL/shoppingList.sql
@@ -14,9 +14,22 @@ CREATE TABLE IF NOT EXISTS shoppingList(
     listID      int             PRIMARY KEY auto_increment,
     CONSTRAINT  FK_userID       FOREIGN KEY (userID) REFERENCES user(userID),
     listName    varchar(50),
-    CONSTRAINT  FK_listName      FOREIGN KEY (itemName) REFERENCES item(itemName),
+    CONSTRAINT  FK_itemName      FOREIGN KEY (itemName) REFERENCES item(itemName),
     quantity    int,
     checkbox    bool
 );
 
-show tables;
+
+-- #user
+-- insert into user (userName) values ('Sufyaan Seedat');
+-- insert into user (userName) values ('John Walker');
+
+-- #item
+-- insert into item (itemName) values ('Bread');
+
+-- #shoppingList
+-- insert into shoppingList (listName, quantity, checkbox) values ('Wednesday list', 2, True);
+        
+select * FROM shoppingList;
+
+
