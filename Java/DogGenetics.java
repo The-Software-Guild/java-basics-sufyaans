0d3ae3c41ebad343a93d6199e6ae576bd4473672
sufyaans <ss1224@student.le.ac.uk>
2022-08-15 10:11:29 +0100

    Final

---
+++ Java/DogGenetics.java
@@ -0,0 +1,41 @@
+package Java;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class DogGenetics {
+
+    public static void main(String[] args)
+    {
+        //Variables
+        String dogName = "";
+        String[] breeds = {"St. Bernard", "Chihuahua", "Dramatic RedNosed Asian Pug", "Common Cur", "King Doberman"};
+        int pecentage = 100;
+        int breedPercent = 0;
+        Random rand = new Random();
+
+        Scanner scanner1 = new Scanner(System.in); 
+        System.out.println("Enter name of dog: ");
+        dogName = scanner1.nextLine();
+        System.out.println("\nWell then, I have this highly reliable report on Sir Fluffy McFlufferkins Esquire's prestigious background right here.");
+        System.out.println("\n" +dogName + " is: ");
+
+        for (int i = 0; i < 5; i++) {
+
+            String breed = "";
+            breed = breeds[i];
+
+            if (i != 4) {
+                breedPercent = rand.nextInt(pecentage);
+                pecentage -= breedPercent;
+            } else {
+                breedPercent = pecentage;
+            }
+            System.out.println(breed + ": " + breedPercent + "%");
+
+        }
+
+        System.out.println("\nWow, that's QUITE the dog!");
+    }
+
+}
---
+++ Java/HealthyHearts.java
@@ -0,0 +1,28 @@
+package Java;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class HealthyHearts {
+
+    public static void main(String[] args){
+        //Variabls
+        int age;
+        int maxHeartRate = 220;
+
+        //Inputs
+        Scanner scanner1 = new Scanner(System.in); 
+        System.out.println("Enter your age: ");
+        age = scanner1.nextInt();
+
+        //Calculations
+        int maxRate = maxHeartRate - age;
+        float maxZone = Math.round(maxRate*0.85);
+        float minZone = Math.round(maxRate*0.50);
+
+        //Outputs
+        System.out.println("\nYour maximum heart rate should be " + maxRate + " beats per minute.");
+        System.out.println("\nYour target HR Zone is " + minZone +"-" + maxZone + " beats per minute.");
+    }
+    
+}
